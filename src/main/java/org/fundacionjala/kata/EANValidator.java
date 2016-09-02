58e20629238f86483df418e20605471dd4172bab
Lourdes Villca Lazaro <LourdesVillca@Fundacion-jala.local>
2016-09-02 14:44:01 -0400

    Refactor the code according the comments

--- src/main/java/org/fundacionjala/kata/EANValidator.java
+++ src/main/java/org/fundacionjala/kata/EANValidator.java
@@ -1,31 +1,16 @@
 package org.fundacionjala.kata;
 
 /**
- * Created by LourdesVillca on 8/26/2016.
+ * Class to Validate EAN number
  */
 public class EANValidator {
 
     public static boolean validate(String number) {
-
         int checksum = 0;
         for (int i = 0; i < number.length() - 1; i++) {
-            if (((i + 1) % 2) == 0) {
-                checksum = checksum + Character.getNumericValue(number.charAt(i)) * 3;
-
-            } else {
-                checksum = checksum + Character.getNumericValue(number.charAt(i)) * 1;
-
-            }
-
+            checksum += ((i + 1) % 2 == 0) ? Character.getNumericValue(number.charAt(i)) * 3 : Character.getNumericValue(number.charAt(i)) * 1;
         }
-
-        if (checksum % 10 == 0) {
-            checksum = 0;
-        } else {
-            checksum = 10 - (checksum % 10);
-        }
-        return (Character.getNumericValue(number.charAt(number.length() - 1)) == checksum)? true:false;
-
+        checksum = checksum % 10 == 0 ? 0 : 10 - (checksum % 10);
+        return Character.getNumericValue(number.charAt(number.length() - 1)) == checksum;
     }
-
 }
