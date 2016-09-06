334e6a4b59c5645b45da3f3175c61c97321c256e
Lourdes Villca Lazaro <LourdesVillca@Fundacion-jala.local>
2016-09-06 13:23:10 -0400

    Fif comments - refactor the code

--- src/main/java/org/fundacionjala/kata/EANValidator.java
+++ src/main/java/org/fundacionjala/kata/EANValidator.java
@@ -7,10 +7,14 @@ public class EANValidator {
 
     public static boolean validate(String number) {
         int checksum = 0;
+        int numberValue;
+
         for (int i = 0; i < number.length() - 1; i++) {
-            checksum += ((i + 1) % 2 == 0) ? Character.getNumericValue(number.charAt(i)) * 3 : Character.getNumericValue(number.charAt(i)) * 1;
+            numberValue = Character.getNumericValue(number.charAt(i));
+            checksum += ((i + 1) % 2 == 0) ? numberValue * 3 : numberValue;
         }
         checksum = checksum % 10 == 0 ? 0 : 10 - (checksum % 10);
-        return Character.getNumericValue(number.charAt(number.length() - 1)) == checksum;
+        final int lastDigit = Character.getNumericValue(number.charAt(number.length() - 1));
+        return lastDigit == checksum;
     }
 }
